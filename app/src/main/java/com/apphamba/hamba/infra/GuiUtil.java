fb241b0c94e8e3b95c299f71a0f7daaa5387575d
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2018-07-11 03:23:55 -0300

    tools.context com nome do pacote redefinido
    E GuiUtil para facilitar as mensagens de erro mostradas na interface

--- app/src/main/java/com/apphamba/hamba/infra/GuiUtil.java
+++ app/src/main/java/com/apphamba/hamba/infra/GuiUtil.java
@@ -1,4 +1,57 @@
 package com.apphamba.hamba.infra;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.widget.Toast;
+
+import com.apphamba.hamba.R;
+
+//Abaixo classe que trata os erros e manda alguma mensagem para mostrar
+//Metodos que chamam e sao mostrados na tela
 public class GuiUtil {
+
+    private static void myToast(Context context, String text, int time){
+        Toast.makeText(context, text, time).show();
+    }
+
+    public static void myToast(Context context, Exception e, int time){
+        GuiUtil.myToast(context, e.getMessage(), time);
+    }
+
+    public static void myToast(Context context, String text){
+        GuiUtil.myToast(context, text, Toast.LENGTH_LONG);
+    }
+
+    public static void myToast(Context context, Exception e){
+        GuiUtil.myToast(context, e.getMessage(), Toast.LENGTH_LONG);
+    }
+
+    public static void myToastShort(Context context, String text){
+        GuiUtil.myToast(context, text, Toast.LENGTH_SHORT);
+    }
+
+    public static void myToastShort(Context context, Exception e){
+        GuiUtil.myToast(context, e.getMessage(), Toast.LENGTH_SHORT);
+    }
+
+    private static void myAlertDialog(Context context, String text, String titulo){
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(context);
+        dialogo.setTitle(titulo);
+        dialogo.setMessage(text);
+        dialogo.setNeutralButton(R.string.dialog_botao_ok, null);
+        dialogo.show();
+    }
+
+    public static void myAlertDialog(Context context, Exception e, String titulo){
+        GuiUtil.myAlertDialog(context, e.getMessage(), titulo);
+    }
+
+    public static void myAlertDialog(Context context, Exception e){
+        GuiUtil.myAlertDialog(context, e.getMessage(), context.getString(R.string.dialog_titulo_erro));
+    }
+
+    public static void myAlertDialog(Context context, String text){
+        GuiUtil.myAlertDialog(context, text, context.getString(R.string.dialog_titulo_erro));
+    }
+
 }
