058f0ea21f99d5639b959dd66af4f3d7fcb16383
BrunoVirgu <34161984+BrunoVirgu@users.noreply.github.com>
2018-07-19 20:11:17 -0300

    vem cmg

---
+++ app/src/androidTest/java/com/apphamba/hamba/titulos/servicos/ServiceTitulosTest.java
@@ -0,0 +1,24 @@
+package com.apphamba.hamba.titulos.servicos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceTitulosTest {
+
+    @Test
+    public void getAllTitulos() {
+    }
+
+    @Test
+    public void converterByteBitMap() {
+    }
+
+    @Test
+    public void converterBitmapDrawable() {
+    }
+
+    @Test
+    public void pegarImagem() {
+    }
+}
