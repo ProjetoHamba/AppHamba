7825efb0b823be9a8adf994e1901e212e91bdeac
Leonardo <leonardo.a.m001@gmail.com>
2018-07-25 22:08:24 -0300

    Tela Favoritos

---
+++ app/src/main/java/com/apphamba/hamba/favoritos.java
@@ -0,0 +1,13 @@
+package com.apphamba.hamba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class favoritos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoritos);
+    }
+}
