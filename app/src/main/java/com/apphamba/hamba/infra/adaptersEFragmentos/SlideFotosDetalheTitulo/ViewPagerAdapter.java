248add95feb1b6148f5c3f7bb563fc5190ab21e5
Kimbelly Ferraz <kimbelly_emanuelle@hotmail.com>
2018-08-06 14:17:09 -0300

    Package SlideFotosDetalheTitulo

--- app/src/main/java/com/apphamba/hamba/infra/adaptersEFragmentos/SlideFotosDetalheTitulo/ViewPagerAdapter.java
+++ app/src/main/java/com/apphamba/hamba/infra/adaptersEFragmentos/SlideFotosDetalheTitulo/ViewPagerAdapter.java
@@ -1,4 +1,4 @@
-package com.apphamba.hamba.infra.adaptersEFragmentos;
+package com.apphamba.hamba.infra.adaptersEFragmentos.SlideFotosDetalheTitulo;
 
 import android.content.Context;
 import android.graphics.Bitmap;
